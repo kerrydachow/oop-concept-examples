79d72b5fc44cdbdde49924822707d7f059a876ab
kerrydachow <kerry_chow@hotmail.ca>
2023-08-28 13:41:25 -0700

    Add inheritance polymorphism example

---
+++ polymorphism/inheritance.py
@@ -0,0 +1,32 @@
+"""
+Polymorphism with Inheritance
+-----------------------------
+
+-   Using method overriding to achieve polymorphism
+"""
+
+
+class Bird:
+    def sound(self):
+        return "Bird sound"
+
+
+class Sparrow(Bird):
+    def sound(self):
+        return "Chirp"
+
+
+class Crow(Bird):
+    def sound(self):
+        return "Caw"
+
+
+def main():
+    sparrow = Sparrow()
+    crow = Crow()
+    print(sparrow.sound())
+    print(crow.sound())
+
+
+if __name__ == "__main__":
+    main()
