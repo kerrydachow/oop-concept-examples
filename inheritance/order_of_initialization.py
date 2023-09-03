146c4316cf4f5a9f849542ab5c4846597c08d085
kerrydachow <kerry_chow@hotmail.ca>
2023-09-02 19:00:46 -0700

    Add order of initialization example

---
+++ inheritance/order_of_initialization.py
@@ -0,0 +1,43 @@
+# Example 1.
+class Animal:
+    def __init__(self):
+        print("Animal")
+
+
+class Dog(Animal):
+    def __init__(self):
+        print("Dog")
+
+print("Example 1:")
+print("-------------------------")
+dog = Dog()
+
+# Example 2.
+class Animal:
+    def __init__(self):
+        print("Animal")
+
+
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+        print("Dog")
+
+print("\nExample 2:")
+print("-------------------------")
+dog = Dog()
+
+# Example 3.
+class Animal:
+    def __init__(self):
+        print("Animal")
+
+
+class Dog(Animal):
+    def __init__(self):
+        print("Dog")
+        super().__init__()
+
+print("\nExample 3:")
+print("-------------------------")
+dog = Dog()
